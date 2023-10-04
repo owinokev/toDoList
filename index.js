f74ceec870dc72d8a9aab059590d5dc74f601c56
Your Name <Kevin/oti>
2023-10-04 12:51:29 +0300

    To do list start

---
+++ index.js
@@ -0,0 +1,22 @@
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var date = new Date()
+var day = date.getDate()
+var month = date.getMonth()
+var dated = monthNames[month]+", " + day
+console.log(dated)
+
+document.querySelector("button.date").innerHTML = dated
+
+
+document.querySelector(".adding").addEventListener("click", function(){
+  var notes = document.querySelector("input").value
+  const node = document.createElement("li")
+  var textnode = document.createTextNode(notes)
+  node.appendChild(textnode)
+  document.getElementById("myList").appendChild(node)
+  console.log(node, textnode)
+
+
+})
